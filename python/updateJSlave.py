f32ae6c44522e1453d4c1a5dc4a3349ab59665e1
liuna-liuna <na.liu01@sap.com>
2016-01-11 14:55:42 +0800

    re-write updateJSlave.py to get a nodename as parameter; debug option.
    Usage: $0 [-n | --nodename] $nodename [-d | --debug]
    ------------------------------
    # to get parameter <argument>	       in jenkins-agent.jnlp to be used as value for  -secret in JENKINS_SLAVE_XML
    # to get parameter <jnlp codebase=...>  in jenkins-agent.jnlp to be used as value for  -jnlpUrl in JENKINS_SLAVE_XML
    # create a JENKINS_SLAVE_XML file

--- python/updateJSlave.py
+++ python/updateJSlave.py
@@ -1,85 +1,80 @@
 # _*_ coding: utf-8 _*_
 
 import urllib
-import libxml2
 import re
 import lxml.etree
 import lxml.builder
-
-url		= "http://${jenkins_server1}:8080/computer/${jenkins_slave1}/slave-agent.jnlp"
-content = urllib.urlopen(url).read()
-print content
-#	print html.getcode()
-#	print html.geturl()
-#	print html.info()
+import contextlib
 
 #
-# example jnlp 
-#	http://${jenkins_server1}:8080/computer/${jenkins_slave1}/slave-agent.jnlp
+# subroutines
 #
-##	<jnlp codebase="http://${jenkins_server1}:8080/computer/${jenkins_slave1}/" spec="1.0+">
-##		<information>
-##			<title>Slave Agent for ${jenkins_slave1}</title>
-##			<vendor>Jenkins project</vendor>
-##			<homepage href="https://jenkins-ci.org/"/>
-##		</information>
-##		
-##		<security>
-##			<all-permissions/>
-##		</security>
-##		
-##		<resources>
-##			<j2se version="1.5+"/>
-##			<jar href="http://${jenkins_server1}:8080/jnlpJars/remoting.jar"/>
-##			<property name="hudson.showWindowsServiceInstallLink" value="true"/>
-##		</resources>
-##		<application-desc main-class="hudson.remoting.jnlp.Main">
-##			<argument>${jenkins_slave1_secret}</argument>
-##			<argument>${jenkins_slave1}</argument>
-##			<argument>-url</argument>
-##			<argument>http://${jenkins_server1}:8080/</argument>
-##		</application-desc>
-##		
-##	</jnlp>
+# level_Prefix: could be INFO, DEBUG.
+def log_print(msg, level_Prefix):
+	print "%s: %s" % (level_Prefix, msg)
+	return
+	
+	
+	
+# get input options
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-n", "--nodename", dest="nodename", help="node name to be checked")
+parser.add_option("-d", "--debug",    dest="debug",    help="output more information for debugging purpose", action="store_true", default=False)
+options, arguments = parser.parse_args()
+if options.nodename:
+	log_print( "The nodename input is: %s" %(options.nodename), "INFO " )
+else:
+	log_print( '''Please input the name of a node to be checked.
+Usage: $0 [-n | --nodename] $nodename [-d | --debug]
+	''', "INFO " )
+	exit(1)
+
+# Constants
+FLAG_DEBUG			= options.debug
+JENKINS_SERVER_URL  = "http://***:8080"
+COMPUTER			= "computer"
+SLAVEAGENT_JNLP		= "slave-agent.jnlp"
+JENKINS_SLAVE_XML	= "jenkins-slave.xml"
+
+url		= JENKINS_SERVER_URL + "/" + COMPUTER + "/" + options.nodename + "/" + SLAVEAGENT_JNLP
+content = "";
+with contextlib.closing( urllib.urlopen(url) ) as x:
+	content = x.read()
+	if FLAG_DEBUG:
+		log_print( "The whole content from the url is:\n\n" + content + "\n", "DEBUG" )
 
 # parse jenkins-agent.jnlp to get parameters: xpath: //jnlp/application-desc/argument
-jnlp		= libxml2.parseMemory(content, len(content))
+jnlp	= lxml.etree.fromstring(content)
+jnlpUrl	= "";
+for element in jnlp.iter():
+	codebase = element.attrib.get('codebase')
+	if (codebase):
+		jnlpUrl = codebase + SLAVEAGENT_JNLP
+		if FLAG_DEBUG:
+			log_print( jnlpUrl, "DEBUG" )
+		break
 
-#only for test #	
-#only for test #	fileName 	= "jnlp.xml"
-#only for test #	jnlp 		= libxml2.parseFile(fileName)
-#only for test #	
-
-# to get parameter <jnlp codebase=...>  in jenkins-agent.jnlp to be used as value for  -jnlpUrl in jenkins-slave.xml
-xpath4codebase	= "//jnlp[@codebase]"
-jnlpUrl			= "";
-for jnlpElement in jnlp.xpathEval(xpath4codebase):
-	for jnlpElement in jnlp.xpathEval(xpath4codebase):
-	    for prop in jnlpElement.properties:
-	        if prop.name == 'codebase':
-				jnlpUrl	 = prop.content + "slave-agent.jnlp"
-				print jnlpUrl
-				break
-
-# to get parameter <argument>	       in jenkins-agent.jnlp to be used as value for  -secret in jenkins-slave.xml
-xpath4arg	= "//jnlp/application-desc/argument"
-secret		= "";
-for arg in jnlp.xpathEval(xpath4arg):
-	argValue = arg.content
-	print argValue
-	
-	# check if a 64 bit hexdigest <- a SHA1 value
-	sha1Pattern 	= '^[a-f0-9]{64}$'
-	sha1PatCompiled = re.compile(sha1Pattern)
-	if bool( sha1PatCompiled.match(argValue) ):
-		secret = argValue
-		print "SHA1 is found: could be the -secret needed in jenkins-slave.xml."
 		
-# to be tested 
-jnlp.freeDoc()
+# to get parameter <argument>	       in jenkins-agent.jnlp to be used as value for  -secret in JENKINS_SLAVE_XML
+# to get parameter <jnlp codebase=...>  in jenkins-agent.jnlp to be used as value for  -jnlpUrl in JENKINS_SLAVE_XML
+secret = "";
+for arg in jnlp.iter():
+	argValue = arg.text
+	if argValue:
+		# check if a 64 bit hexdigest <- a SHA1 value
+		sha1Pattern 	= '^[a-f0-9]{64}$'
+		sha1PatCompiled = re.compile(sha1Pattern)
+		if bool( sha1PatCompiled.match(argValue) ):
+			secret = argValue
+			if FLAG_DEBUG:
+				log_print( "SHA1 is found: " + secret + "\n       It could be the -secret needed in " + JENKINS_SLAVE_XML, "DEBUG" )
+			break
+	else:
+		continue
 
 
-# create a jenkins-slave.xml file 
+# create a JENKINS_SLAVE_XML file 
 E = lxml.builder.ElementMaker()
 SERVICE = E.service
 ID = E.id
@@ -100,11 +95,14 @@ theDoc = SERVICE(
 	ONFAILURE('', action = 'restart'),
 )
 
-with open("jenkins-slave.xml", "w") as f:
+xmlFileName = JENKINS_SLAVE_XML + "_for_" + options.nodename
+with open(xmlFileName, "w") as f:
 	f.write( lxml.etree.tostring(theDoc, pretty_print=True) )
+	log_print( "File " + xmlFileName + " is created.\n       It could be used to connect between a Jenkins server and a node.", "INFO " )
 
-	
-	
-	
+exit(0)
+
+
+
 
 
