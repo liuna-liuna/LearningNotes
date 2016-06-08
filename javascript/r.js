5c6fc51df9534d45784c1b686bde20081829cc87
liuna-liuna <na.liu01@sap.com>
2016-06-08 08:00:29 +0800

    Create r.js
    1) using r.js to package
    http://www.cnblogs.com/rubylouvre/p/4262569.html
    
    2) source code, brief introduction
    	https://github.com/requirejs/r.js/blob/master/README.md
    	
    	related defs:
    	AMD https://github.com/amdjs/amdjs-api/blob/master/AMD.md
    	use with AMD and requirejs in node: http://requirejs.org/docs/node.html

---
+++ javascript/r.js
@@ -0,0 +1,12 @@
+#
+# r.js
+#
+1) using r.js to package
+http://www.cnblogs.com/rubylouvre/p/4262569.html
+
+2) source code, brief introduction
+	https://github.com/requirejs/r.js/blob/master/README.md
+	
+	related defs:
+	AMD https://github.com/amdjs/amdjs-api/blob/master/AMD.md
+	use with AMD and requirejs in node: http://requirejs.org/docs/node.html
